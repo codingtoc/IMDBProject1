96879a9154d5820b0d7c2df6e2d0c5ad06353c16
codingtoc <codingtoc@gmail.com>
2024-10-31 18:31:55 +0900

    moviesList1 Component에서 getMovies Apex Class Method 호출

--- force-app/main/default/lwc/moviesList1/moviesList1.js
+++ force-app/main/default/lwc/moviesList1/moviesList1.js
@@ -1,9 +1,12 @@
-import { LightningElement } from "lwc";
+import { LightningElement, wire } from "lwc";
+import getMovies from "@salesforce/apex/IMDBController1.getMovies";
 
 export default class MoviesList1 extends LightningElement {
   enteredText = "";
   searchText = "";
   showText = "Please Enter Valid Movie Name";
+
+  movies = [];
 
   handleChange(event) {
     this.enteredText = event.target.value;
@@ -11,5 +14,22 @@ export default class MoviesList1 extends LightningElement {
 
   handleClick(event) {
     this.searchText = this.enteredText;
+  }
+
+  @wire(getMovies, { searchText: "$searchText" })
+  fetchMovies(response) {
+    if (response.data) {
+      let data = JSON.parse(response.data);
+      if (data.success) {
+        this.movies = data.result;
+        this.showText = "";
+      } else {
+        this.movies = [];
+        this.showText = "Please Enter Valid Movie Name";
+      }
+    } else if (response.error) {
+      console.log("Error occured while searching movies: " + response.error);
+      this.showText = "Error occured while searching movies: " + response.error;
+    }
   }
 }
