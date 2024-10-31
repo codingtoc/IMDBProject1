ad149b4546f86ab5250dbadea0a357e2e34d781a
codingtoc <codingtoc@gmail.com>
2024-10-31 15:01:30 +0900

    Search Bar와 Search Button 추가

--- force-app/main/default/lwc/moviesList1/moviesList1.js
+++ force-app/main/default/lwc/moviesList1/moviesList1.js
@@ -1,3 +1,15 @@
-import { LightningElement } from 'lwc';
+import { LightningElement } from "lwc";
 
-export default class MoviesList1 extends LightningElement {}
+export default class MoviesList1 extends LightningElement {
+  enteredText = "";
+  searchText = "";
+  showText = "Please Enter Valid Movie Name";
+
+  handleChange(event) {
+    this.enteredText = event.target.value;
+  }
+
+  handleClick(event) {
+    this.searchText = this.enteredText;
+  }
+}
